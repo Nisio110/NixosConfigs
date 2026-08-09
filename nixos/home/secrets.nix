b3526bb0f3bd0d6c7ed37c8f555eee536187740e
Nisio110 <laneoisin64@gmail.com>
2026-08-09 21:50:55 +0100

    NixOS Rebuild: Generation 320

--- nixos/home/secrets.nix
+++ nixos/home/secrets.nix
@@ -11,13 +11,8 @@
   sops.secrets = {
     github_token.path = "${secretsDir}/github_token";
     brave_api_key.path = "${secretsDir}/brave_api_key";
-    kimi_api_key.path = "${secretsDir}/kimi_api_key";
     openrouter_api_key.path = "${secretsDir}/openrouter_api_key";
     openai_api_key.path = "${secretsDir}/openai_api_key";
-    proxmox_host.path = "${secretsDir}/proxmox_host";
-    proxmox_token_id.path = "${secretsDir}/proxmox_token_id";
-    proxmox_token_secret.path = "${secretsDir}/proxmox_token_secret";
-    browser_control.path = "${secretsDir}/browser_control";
     joplin_postgres.path = "${secretsDir}/joplin_postgres";
   };
 }
