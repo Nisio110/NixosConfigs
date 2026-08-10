ddb46c67458e980059496fee2bae76c38d6f59fb
Nisio110 <laneoisin64@gmail.com>
2026-08-11 00:06:11 +0100

    NixOS Rebuild: Generation 335

--- nixos/modules/home-server/notes.nix
+++ nixos/modules/home-server/notes.nix
@@ -12,7 +12,7 @@
     templates.".env.joplin" = {
       mode = "0400";
       content = ''
-        POSTGRES_PASSWORD=${config.sops.joplin_postgres}
+        POSTGRES_PASSWORD=${config.sops.secrets.joplin_postgres}
         APP_BASE_URL=http://notes.tetocorp.ie;
         DB_CLIENT=pg;
         NODE_ENV=production;
