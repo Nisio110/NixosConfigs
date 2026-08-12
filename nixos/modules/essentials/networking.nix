91a85a28bbf580d489f5ee9042b835621cd0b952
Nisio110 <laneoisin64@gmail.com>
2026-08-12 21:48:45 +0100

    NixOS Rebuild: Generation 372

--- nixos/modules/essentials/networking.nix
+++ nixos/modules/essentials/networking.nix
@@ -2,24 +2,23 @@
 {
   networking = {
     hostName = "hdesktop";
-    networkmanager.enable = true;
     nameservers = [ "1.1.1.1" "1.0.0.1" "8.8.8.8" ];
+    defaultGateway = {
+      address = "192.168.0.1";
+    };
+    networkmanager.enable = true;
 
     firewall.enable = false;
-    interfaces."eno1".ipv4.addresses = [
-      {
-        address = "192.168.0.19";
-        prefixLength = 24;
-      }
-      {
-        address = "192.168.0.21";
-        prefixLength = 24;
-      }
-      {
-        address = "192.168.0.22";
-        prefixLength = 24;
-      }
-    ];
+    
+    interfaces."eno1" = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.0.19";
+          prefixLength = 24;
+        }
+      ];
+    };
   };
 
   services = {
