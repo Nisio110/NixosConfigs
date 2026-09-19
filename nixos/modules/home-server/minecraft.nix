7639503e2aa164c0add1292e1e87a68f2ae67aca
Nisio110 <laneoisin64@gmail.com>
2026-09-19 23:18:51 +0100

    NixOS Rebuild: Generation 408

--- nixos/modules/home-server/minecraft.nix
+++ nixos/modules/home-server/minecraft.nix
@@ -8,7 +8,7 @@ in
 {
   services.minecraft-server.enable = true;
   systemd.tmpfiles.rules = [
-    "L+ /var/lib/minecraft/server-icon.png - - - - ${tetoPlush}"
+    "C+ /var/lib/minecraft/server-icon.png - - - - ${tetoPlush}"
   ];
 
   sops.secrets.mc-rcon-password = {};
