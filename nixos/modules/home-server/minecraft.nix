91d1152a70502c1f6252b3546930b7fac268156e
Nisio110 <laneoisin64@gmail.com>
2026-09-20 00:52:36 +0100

    NixOS Rebuild: Generation 422

--- nixos/modules/home-server/minecraft.nix
+++ nixos/modules/home-server/minecraft.nix
@@ -19,6 +19,7 @@ in
   sops.secrets.mc-rcon-password = {};
 
   services.minecraft-server = {
+    package = pkgs.papermc;
     eula = true;
     dataDir = "/var/lib/minecraft";
     jvmOpts = "-Xmx4096M -Xms1024M";
