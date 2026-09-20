b1c7fc86754b08d28f9928fbc909876b9b19c46f
Nisio110 <laneoisin64@gmail.com>
2026-09-20 15:31:01 +0100

    NixOS Rebuild: Generation 449

--- home-server/nginx.nix
+++ home-server/nginx.nix
@@ -162,7 +162,7 @@ in
         inherit sslCertificate;
         inherit sslCertificateKey;
         locations."/" = {
-          proxyPass = "https://192.168.20:47990";
+          proxyPass = "https://127.0.0.1:5051";
           proxyWebsockets = true;
           extraConfig = "proxy_set_header Origin $sunshine_ui_origin;";
         };
@@ -182,7 +182,7 @@ in
       }
     '';
     streamConfig = ''
-      # DNS needs both UDP (standard queries) and TCP (large/truncated responses).
+      # DNS - TCP & UDP
       server {
         listen 1053 udp;
         proxy_pass 127.0.0.1:53;
@@ -192,6 +192,12 @@ in
       server {
         listen 1053;
         proxy_pass 127.0.0.1:53;
+      }
+
+      # Sunshine
+      server {
+        listen 5050 udp
+        proxy_pass 127.0.0.1:5050
       }
     '';
   };
--- home-server/stream.nix
+++ home-server/stream.nix
@@ -50,9 +50,9 @@ in
       minimum_fps_target = "60";
 
       # == network ==
-      bind_address = "192.168.0.20";
+      bind_address = "127.0.0.1";
       address_family = "ipv4";
-      port = 47989;
+      port = 5050;
       origin_web_ui_allowed = "wan";
       csrf_allowed_origins = "https://stream.tetocorp.ie http://stream.tetocorp.ie http://127.0.0.1";
       wan_encryption_mode = "0";
