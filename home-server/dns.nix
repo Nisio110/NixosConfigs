0f6aac11c4a03c70c84bf9d97f541a712a916ed5
Nisio110 <laneoisin64@gmail.com>
2026-09-20 14:49:47 +0100

    NixOS Rebuild: Generation 440

--- home-server/dns.nix
+++ home-server/dns.nix
@@ -6,7 +6,7 @@
     settings = {
       server = {
         ip-freebind  = true; 
-        interface    = [ "127.0.0.1" "192.168.0.19" ];
+        interface    = [ "127.0.0.1" "192.168.0.19" "192.168.0.20" ];
         local-zone = [ ''"tetocorp.ie." redirect'' ];
 
         access-control = [
