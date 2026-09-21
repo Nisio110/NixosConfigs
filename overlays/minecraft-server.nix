f7c66c8f7390e0359e4311aac4a686d5d7a811e3
Nisio110 <laneoisin64@gmail.com>
2026-09-21 21:57:44 +0100

    NixOS Rebuild: Generation 471

--- overlays/minecraft-server.nix
+++ overlays/minecraft-server.nix
@@ -3,7 +3,7 @@
     (final: prev: {
       minecraft-server = prev.minecraft-server.overrideAttrs (old: {
         src = final.fetchurl {
-          url = "https://meta.fabricmc.net/v2/versions/loader/26.3/0.19.5/1.1.2/server/jar";
+          url = "https://meta.fabricmc.net/v2/versions/loader/26.2/0.19.5/1.1.2/server/jar";
           sha256 = "sha256-C1atVNdiFy6Lh0jkZ/WE8HHk3t7Nk9wzbPLGiDfnkL4=";
         };
       });
